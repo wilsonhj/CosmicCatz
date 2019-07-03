52174966a9ea1db49e98598108e40c0fdef7cb73
James Yamagata <wilsonhj@uci.edu>
2019-07-03 15:27:17 -0700

    flip one card functionality implemented: addClass(hidden)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
+$(document).ready(initializeApp);
 
+
+function initializeApp() {
+    $('.lfz-card').click(handleCardClick);
+
+}
+
+function handleCardClick(event){
+    console.log(event);
+    // $(event.currentTarget).hide();
+    $(event.currentTarget).addClass("hidden");
+
+}
