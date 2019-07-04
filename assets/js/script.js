570ce0d9ca7a3ef9eeeed3c7d8b5cc6490100d3a
James Yamagata <wilsonhj@uci.edu>
2019-07-03 21:13:41 -0700

    wrote match two card functions with setTimeout in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
 $(document).ready(initializeApp);
-
+var firstCardClicked = null;
+var secondCardClicked = null; 
+var matches = null;
 
 function initializeApp() {
-    $('.lfz-card').click(handleCardClick);
-
+    $('.card-container').click(handleCardClick);
 }
 
 function handleCardClick(event){
     console.log(event);
     // $(event.currentTarget).hide();
-    $(event.currentTarget).addClass("hidden");
-
+    $(event.currentTarget).find(".lfz-card").addClass("hidden");
+    debugger;
+    if(firstCardClicked === null){
+        firstCardClicked = $(event.currentTarget);
+        return;
+        
+    }
+    secondCardClicked = $(event.currentTarget);
+    var secondCardSource = secondCardClicked.find(".match-card").css("background-image");
+    var firstCardSource = firstCardClicked.find(".match-card").css("background-image");
+    if (firstCardSource === secondCardSource) {
+        console.log("cards match");
+        matches += 1;
+    }
+    else {
+        setTimeout(() => {
+            firstCardClicked.find(".lfz-card").removeClass("hidden");
+            secondCardClicked.find(".lfz-card").removeClass("hidden");
+            firstCardClicked = null;
+            secondCardClicked = null;
+        }, 1500);
+    }
+    
 }
