cce46a932ed9b5542b9534d198464c3b1dda31b1
James Yamagata <wilsonhj@uci.edu>
2019-07-06 17:01:31 -0700

    match two cards. mouse click off. hide matched cards. hubble background. space Catz imgs.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,25 +4,38 @@ var secondCardClicked = null;
 var matches = null;
 
 function initializeApp() {
-    $('.card-container').click(handleCardClick);
+    // $('.card-container').click(handleCardClick);
+    // $('.lfz-card').on('click', handleCardClick);
+    $('.card-container').on('click', handleCardClick);
 }
 
 function handleCardClick(event){
     console.log(event);
-    // $(event.currentTarget).hide();
     $(event.currentTarget).find(".lfz-card").addClass("hidden");
-    debugger;
     if(firstCardClicked === null){
         firstCardClicked = $(event.currentTarget);
         return;
         
     }
     secondCardClicked = $(event.currentTarget);
+    $('.card-container').off('click', handleCardClick); // toggle off click
     var secondCardSource = secondCardClicked.find(".match-card").css("background-image");
     var firstCardSource = firstCardClicked.find(".match-card").css("background-image");
     if (firstCardSource === secondCardSource) {
         console.log("cards match");
         matches += 1;
+        // hides elements if matched
+        
+        // firstCardClicked.addClass("hidden"); 
+        // secondCardClicked.addClass("hidden");
+        firstCardClicked.hide(3000);
+        secondCardClicked.hide(3000);
+        // resets cards to null
+        firstCardClicked = null;
+        secondCardClicked = null;
+        
+        
+        $('.card-container').on('click', handleCardClick);
     }
     else {
         setTimeout(() => {
@@ -30,7 +43,9 @@ function handleCardClick(event){
             secondCardClicked.find(".lfz-card").removeClass("hidden");
             firstCardClicked = null;
             secondCardClicked = null;
+            $('.card-container').on('click', handleCardClick);
         }, 1500);
+        
     }
     
 }
